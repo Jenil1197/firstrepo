961358289116dfc6b8a553044f2cfb77cf8d3f8f
Jenil1197 <sutarije@sheridancollege.ca>
2024-09-16 17:20:45 -0400

    kam karna

--- src/demomondayevening/DemoMondayEvening.java
+++ src/demomondayevening/DemoMondayEvening.java
@@ -14,7 +14,7 @@ public class DemoMondayEvening {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Potano laile");
+        System.out.println("hi there");
     }
     
 }
