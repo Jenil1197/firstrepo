ab170bd07cbee57328deccd019f78a5aeb1c2dcb
Jenil1197 <sutarije@sheridancollege.ca>
2024-09-16 16:41:32 -0400

    kam karna

---
+++ src/demomondayevening/DemoMondayEvening.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demomondayevening;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DemoMondayEvening {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Potano laile");
+    }
+    
+}
